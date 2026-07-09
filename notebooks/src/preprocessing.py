bd7927c8e23ba741e7a6c7c9f8fe87617ede5b2d
Rodrigo Emerson Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2026-07-09 13:11:01 -0500

    feat(ml): profesionalizar pipeline con GridSearchCV y SelectKBest

--- notebooks/src/preprocessing.py
+++ notebooks/src/preprocessing.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 
 def fase_3_preprocesamiento(datos: pd.DataFrame):
@@ -19,8 +18,4 @@ def fase_3_preprocesamiento(datos: pd.DataFrame):
     caracteristicas_balanceadas, objetivo_balanceado = smote.fit_resample(caracteristicas, objetivo)
     print('  -> Dividiendo datos en Entrenamiento (80%) y Prueba (20%)...')
     X_entrenamiento, X_prueba, y_entrenamiento, y_prueba = train_test_split(caracteristicas_balanceadas, objetivo_balanceado, test_size=0.2, random_state=42)
-    print('  -> Estandarizando variables numéricas (StandardScaler)...')
-    escalador = StandardScaler()
-    X_entrenamiento_escalado = escalador.fit_transform(X_entrenamiento)
-    X_prueba_escalado = escalador.transform(X_prueba)
-    return (X_entrenamiento_escalado, X_prueba_escalado, y_entrenamiento, y_prueba, escalador)
+    return (X_entrenamiento, X_prueba, y_entrenamiento, y_prueba)
